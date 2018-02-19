5dacafac20eee2cd501445c405b7bc8ba95c1477
Marco Leichsenring <marco.leichsenring@idealo.de>
2018-02-19 10:53:21 +0100

    Fixes #3 enable handler listener by exposing it as @Component

--- src/main/java/de/mle/sandbox/service/ProductOpinionEventListener.java
+++ src/main/java/de/mle/sandbox/service/ProductOpinionEventListener.java
@@ -1,21 +1,23 @@
 package de.mle.sandbox.service;
 
 import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
+import org.springframework.stereotype.Component;
 
 import de.mle.sandbox.domain.ProductOpinion;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class ProductOpinionEventListener extends AbstractRepositoryEventListener<ProductOpinion> {
 	@Override
 	public void onBeforeCreate(ProductOpinion productOpinion) {
-		log.warn("Before creating product opinion {}", productOpinion);
+		log.info("Before creating product opinion {}", productOpinion);
 	}
 
 	@Override
 	public void onAfterCreate(ProductOpinion productOpinion) {
-		log.warn("After creating product opinion {}", productOpinion);
+		log.info("After creating product opinion {}", productOpinion);
 	}
 
 }
